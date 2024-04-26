11444691c94b185b563c3f4eb96b6fd2e94f0971
maf1oznic <127044787+maf1oznic@users.noreply.github.com>
2024-04-26 18:47:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = require("config")
 const fileUpload = require("express-fileupload")
 const authRouter = require("./routes/auth.routes")
 const fileRouter = require("./routes/file.routes")
+const userRoute = require('./routes/userRoute')
 const app = express()
 const PORT = process.env.PORT || config.get('serverPort')
 const corsMiddleware = require('./middleware/cors.middleware')
@@ -19,6 +20,7 @@ app.use(filePathMiddleware(path.resolve(__dirname, 'files')))
 app.use(express.json())
 app.use("/api/auth", authRouter)
 app.use("/api/files", fileRouter)
+app.use('/api/user', userRoute);
 
 const start = async () => {
     try {
